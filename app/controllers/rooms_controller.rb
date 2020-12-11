5c5c1b22a8216fb7ce36614084d0ac400825257c
keroropper <somato.0311@gmail.com>
2020-12-11 09:50:28 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,19 +4,21 @@ def new
   @room = Room.new #値が空のRoomインスタンスを@roomに代入
 end
 
-
-
-
-
-
+def create
+  @room = Room.new(room_params)
+  if @room.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
 
 
 private
 
-
-
-
-
+def room_params
+  params.require(:room).permit(:name, user_ids: [])
+end
 
 
 end
