5199c0c4671ee3cb8195ade45e5dcdadfc357629
keroropper <somato.0311@gmail.com>
2020-12-11 18:24:06 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :users, only: [:edit,:update]
-  resources :rooms, only: [:new, :create] do  #ネストすることによって、メッセージを投稿する際、どのルームで投稿されたメッセージなのかをパスから判断できる
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create, :destroy] do  #ネストすることによって、メッセージを投稿する際、どのルームで投稿されたメッセージなのかをパスから判断できる
     resources :messages, only: [:index, :create]
     #roomsが親で、messagesが子という親子関係になるので、チャットルームに属しているメッセージという意味になる
   end
